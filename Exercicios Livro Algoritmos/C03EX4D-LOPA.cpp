888bed61765ba51f9516a50971f22acaa395c61c
Maycovisk <90663036+maycovisk@users.noreply.github.com>
2022-04-11 13:14:38 -0300

    Add files via upload

--- Exercicios Livro Algoritmos/C03EX4D-LOPA.cpp
+++ Exercicios Livro Algoritmos/C03EX4D-LOPA.cpp
@@ -7,20 +7,20 @@ const int L = 12;
 
 int main(void)
 {
-  float T, V, D,LU;
+  float TP, VEL, DIS,LIT;
 
   cout << "Digite o tempo de viagem...: "; //como é calculado o tempo? minutos, horas?
-  cin >> T;
+  cin >> TP;
   cin.ignore(80,'\n');
 
   cout << "Digite a velocidade media..: ";
-  cin >> V;
+  cin >> VEL;
   cin.ignore(80,'\n');
 
-  D = T * V;
-  LU = D / L;
+  DIS = TP * VEL;
+  LIT = DIS / L;
 
-  cout << "A quantidade de litro usado nesta viagem foi de...: " << LU << endl;
+  cout << "A quantidade de litro usado nesta viagem foi de...: " << LIT << endl;
 
   cout << endl;
   cout << "Tecle <Enter> para encerrar....";
